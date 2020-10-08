138ab0cb8823da4c0c708efb2523be4c2a93436f
lavmark <lavonnamark@u.yale-nus.edu.sg>
2020-10-08 09:19:32 -0400

    fixed high pup

--- scripts/parcor_undirected/beforetrial_corr_incorr/step2_averageplots_highpupil_corrincorr.m
+++ scripts/parcor_undirected/beforetrial_corr_incorr/step2_averageplots_highpupil_corrincorr.m
@@ -40,7 +40,7 @@ for i=1:length(animals)
     trial_highpup_incorrect_cat_betweenness=cat(2, trial_highpup_incorrect_cat_betweenness,trial_highpup_incorrect.cent_corr.betweenness);       
     trial_highpup_incorrect_cat_pagerank=cat(2, trial_highpup_incorrect_cat_pagerank,trial_highpup_incorrect.cent_corr.pagerank); 
     trial_highpup_incorrect_cat_eigenvector=cat(2, trial_highpup_incorrect_cat_eigenvector,trial_highpup_incorrect.cent_corr.eigenvector); 
-    diffusionmap_twoconditions(trial_highpup_correct.W_corr,trial_highpup_incorrect.W_corr,trial_highpup_correct.cent_corr.eigenvector,trial_highpup_incorrect.cent_corr.eigenvector,condition1,condition2,animal,'eigenvector')    
+    diffusionmap_twoconditions(trial_highpup_correct.W_corr,trial_highpup_incorrect.W_corr,trial_highpup_correct.cent_corr.eigenvector,trial_highpup_incorrect.cent_corr.eigenvector,condition1,condition2,animal,'eigenvector')
     clearvars trial_highpup_correct trial_highpup_incorrect
 end
 [~,textt]=xlsread('X:\Hadas\Meso-imaging\Antara\preprocessing\meso_processing-master\parcellation\AllenParcellationLan\allanParcellationTiffs\subregion_list.csv');
@@ -55,8 +55,8 @@ parcels_names=textt(finalindex,1);
 % graph_overlay_allen_paired(outputpth, trial_highpup_correct_cat_pagerank,trial_highpup_incorrect_cat_pagerank,'trial_highpup_correct_trial_highpup_incorrect','pagerank_centrality','trial_highpup_correct vs trial_highpup_incorrect pagerank Centrality (spon)',parcels_names,length(animals));
 % graph_overlay_allen_paired(outputpth, trial_highpup_correct_cat_eigenvector,trial_highpup_incorrect_cat_eigenvector,'trial_highpup_correct_trial_highpup_incorrect','eigenvector_centrality','trial_highpup_correct vs trial_highpup_incorrect eigenvector Centrality (spon)',parcels_names,length(animals));
 % graph_overlay_allen_paired(outputpth, trial_highpup_correct_cat_betweenness,trial_highpup_incorrect_cat_betweenness,'trial_highpup_correct_trial_highpup_incorrect','betweenness_centrality','trial_highpup_correct vs trial_highpup_incorrect betweenness Centrality (spon)',parcels_names,length(animals));
-averaged_differences_eig = permuted_centrality('trial_highpup_correct','trial_highpup_incorrect');
-graph_overlay_allen_paired_permuted(averaged_differences_eig,outputpth, trial_highpup_correct_cat_eigenvector,trial_highpup_incorrect_cat_eigenvector,'trial_highpup_correct_trial_highpup_incorrect','eigenvector_centrality','trial_highpup_correct vs trial_highpup_incorrect eigenvector Centrality (spon)',parcels_names,length(animals));
+[avg_cond1,avg_cond2] = permuted_centrality('trial_highpup_correct','trial_highpup_incorrect');
+graph_overlay_allen_paired_permuted(avg_cond1,avg_cond2,outputpth, trial_highpup_correct_cat_eigenvector,trial_highpup_incorrect_cat_eigenvector,'trial_highpup_correct_trial_highpup_incorrect','eigenvector_centrality','trial_highpup_correct vs trial_highpup_incorrect eigenvector Centrality (spon)',parcels_names,length(animals));
 
 graph_overlay_allen_notpaired(outputpth, trial_highpup_correct_cat_degree,trial_highpup_incorrect_cat_degree,'trial_highpup_correct_trial_highpup_incorrect','degree_centrality','trial_highpup_correct vs trial_highpup_incorrect degree Centrality (spon)',parcels_names,length(animals));
 graph_overlay_allen_notpaired(outputpth, trial_highpup_correct_cat_closeness,trial_highpup_incorrect_cat_closeness,'trial_highpup_correct_trial_highpup_incorrect','closeness_centrality','trial_highpup_correct vs trial_highpup_incorrect closeness Centrality (spon)',parcels_names,length(animals));
