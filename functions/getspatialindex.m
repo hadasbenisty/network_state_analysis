740d922ba909246e3ae4872e950e660b7de9663f
lavmark <lavonnamark@u.yale-nus.edu.sg>
2020-11-04 06:07:45 -0500

    added spat indx

---
+++ functions/getspatialindex.m
@@ -0,0 +1,8 @@
+function [indx_twentythree,indx_fiftysix] = getspatialindex
+%if data is already subsetted by final index
+indx_twentythree=[1,14,23];
+%if there still are 56 parcels
+indx_fiftysix=[2,34,52];
+
+end
+
