958b3d64b4a7a9b5fd4975899f4285c51d3af695
lavmark <lavonnamark@u.yale-nus.edu.sg>
2021-01-13 09:13:56 -0500

    regress

--- scripts/regression_code_demoall.m
+++ scripts/regression_code_demoall.m
@@ -4,28 +4,28 @@ cd('C:\Users\SuperComp-HigleyLab\Documents\Lav\network_state_analysis\scripts')
 addpath(genpath('../pre_processing'));
 addpath(genpath('../meta_data_processing'));
 addpath(genpath('../../utils'));
-addpath('X:\Hadas\Meso-imaging\Antara\results\code\regression')
+addpath(genpath('../network_state_analysis/functions'));
+
 datapath = 'X:\Hadas\Meso-imaging\Antara\data\Antara\AnalyzedData';
 imagingpath='X:\Hadas\Meso-imaging\Antara\final_preprocess\alldata';
 spike2path = 'X:\CardinLab\Antara\AnalyzedData\';
 animals_db = get_animals_meta_data_by_csv;
 procdatapath = 'X:\Hadas\Meso-imaging\crispr\meso_results\ProcessingDirectory';
 %% regression and ridge regression
-%outputMat_grabs_regular=
-%outputMat_grabs_ridge=? size
+%k=4,9,10
 typesvals = unique(animals_db.type_list);
-for ti = 1:length(typesvals)
+all_results_type=[];%NaN(4,56,1);
+for ti = 1%:length(typesvals)
     curtype = animals_db.type_lut{ti};
     animalsinds = find(animals_db.type_list==ti);
-    all_results_type=[];
-    for kj=1:length(animalsinds)
+    for kj=4%:length(animalsinds)
         k=animalsinds(kj);
         if animals_db.isgoodpupil_list(k)==find(strcmp(animals_db.isgoodpupil_lut, 'GOOD'))&&animals_db.isimagingood_list(k)==find(strcmp(animals_db.isimagingood_lut, 'GOOD'))
             %extract_sustained_state(datapath, procdatapath, spike2path, animals_db.folder_list{k});
             tic
             isair=animals_db.sessionsid_list(k)==1;
             [outputMat_gcamp_regular, outputMat_grabs_ridge] = ...
-                returnCVRSquared(fullfile(datapath,animals_db.folder_list{k}),fullfile(imagingpath,animals_db.folder_list{k}),fullfile(spike2path,animals_db.folder_list{k}),isair,25);
+                returnCVRSquared(animals_db.folder_list{k},k,fullfile(datapath,animals_db.folder_list{k}),fullfile(imagingpath,animals_db.folder_list{k}),fullfile(spike2path,animals_db.folder_list{k}),isair,1);
             toc
             save(fullfile(datapath,animals_db.folder_list{k},'regression_results'),'outputMat_gcamp_regular');
             all_results_type=cat(3,all_results_type,outputMat_gcamp_regular);
@@ -36,5 +36,19 @@ for ti = 1:length(typesvals)
     clearvars all_results_type
 end
 
-
-
+%%plot the beta values
+allpertype=load(fullfile(datapath,strcat(curtype,'all_regression_results')));
+%order: allVars,pupil,face,wheel;   
+load('X:\Hadas\Meso-imaging\lan\results\code\Functions\brain_mask.mat')
+parcelsallen=load('X:\Hadas\Meso-imaging\Antara\preprocessing\parcells_updated121519.mat');
+%all
+figure;
+regression_heatmap(brain_mask,parcelsallen.parcells_new.indicators,squeeze(allpertype.all_results_type(1,:,:)),'All Vars')
+%Pupil
+figure;
+regression_heatmap(brain_mask,parcelsallen.parcells_new.indicators,squeeze(allpertype.all_results_type(2,:,:)),'Pupil')
+figure;
+regression_heatmap(brain_mask,parcelsallen.parcells_new.indicators,squeeze(allpertype.all_results_type(3,:,:)),'Face')
+figure;
+regression_heatmap(brain_mask,parcelsallen.parcells_new.indicators,squeeze(allpertype.all_results_type(4,:,:)),'Wheel')
+close all;
--- scripts/returnCVRSquared.m
+++ scripts/returnCVRSquared.m
@@ -1,5 +1,6 @@
 function [outputMat_grabs_regular, outputMat_grabs_ridge] = ...
-    returnCVRSquared(dataFolderPath,imagingdatapath,spike2path,airpuff_trial,howManyFacePCs)
+    returnCVRSquared(animal,k,dataFolderPath,imagingdatapath,spike2path,airpuff_trial,howManyFacePCs)
+%dbstop if warning
 tic
 %**************************************************************************
 % airpuff_trial input is 1 if it is an airpuff trial, 0 if not
@@ -31,18 +32,21 @@ tic
 % load in data
 % *************************************************************************
 
-cd(dataFolderPath); % change directory
+%cd(dataFolderPath); % change directory
 % detrending params
 params.deterend.filtLen = 150;
 params.deterend.filtcutoff = 0.001;
 params.deterend.method = 'FIR';
 
 % imaging_data = load('final_dFoF.mat'); % load in full imaging data <-
+if exist(fullfile(imagingdatapath,'Ca_traces_spt_patch14_Allen_dfff.mat'))>0&&exist(fullfile(dataFolderPath,'smrx_signals_v2.mat'))>0
+    
 dFoF_parcells = load(fullfile(imagingdatapath,'Ca_traces_spt_patch14_Allen_dfff.mat')); %<- parcels data
+smrx_sigs = load(fullfile(dataFolderPath,'smrx_signals_v2.mat'));
+
 dFoF_parcells_data=dFoF_parcells.parcels_time_trace;
 blue_parcels = dFoF_parcells_data;
 %add dfof into the same folders later 
-smrx_sigs = load('smrx_signals_v2.mat');
 %smrx_sigs.timestamps.timaging=smrx_sigs.timestamps.timaging(params.deterend.filtLen/2:end);
 if size(blue_parcels,2)==length(smrx_sigs.timestamps.timaging)
 elseif size(blue_parcels,2)<length(smrx_sigs.timestamps.timaging)
@@ -97,8 +101,16 @@ pupil_sig=zscore(pupil_interp);
 % pupil_interp = interp1(pupil_time,pupil,smrx_sigs.timestamps.timaging);
 % pupil_sig = zscore(pupil_interp);
 
+%make facemap and pupil cam ticks same length
+
 % 2) face PC1**************************************************************
 wholeFaceSVD_time = pupil_time;
+if size(proc_output.proc.motSVD{1,1},1)==length(wholeFaceSVD_time)
+elseif size(proc_output.proc.motSVD{1,1},1)<length(wholeFaceSVD_time)
+    wholeFaceSVD_time=wholeFaceSVD_time(1:size(proc_output.proc.motSVD{1,1},1));
+elseif size(proc_output.proc.motSVD{1,1},1)>length(wholeFaceSVD_time)
+    proc_output.proc.motSVD{1,1}=proc_output.proc.motSVD{1,1}(1:length(smrx_sigs.timing.pupilcamstart),:);
+end
 wholeFaceSVD_interp = zeros(length(smrx_sigs.timestamps.timaging), 100);
     for i = 1:100
         tmp = proc_output.proc.motSVD{1,1}(:,i);
@@ -132,8 +144,9 @@ blue_parcels_z = zeros(size(blue_parcels,1), size(blue_parcels,2));
 if airpuff_trial
     % TIMESTAMPS OF AIRPUFFS (seconds) smrx_sigs.timing.allairpuffstart
     % remove 10 second before and 60 seconds after airpuff (can change)
-    time_to_remove_before = 10*10; %s * 10 Hz
-    time_to_remove_after = 60*10; %s * 10 Hz
+    time_to_remove_before = 1*10; %s * 10 Hz
+    time_to_remove_after = 6*10; %s * 10 Hz
+    %lav changed to 1 and 6.
     
     %1 find indicies
     %2 make binary matrix indicating what to delete
@@ -161,6 +174,8 @@ if airpuff_trial
     wheel(airpuff_delete == 1) = [];
 
     blue_parcels_z(:,airpuff_delete == 1) = [];
+    
+    blue_parcels(:,airpuff_delete == 1) = [];
       
 end    
     
@@ -173,14 +188,14 @@ pupil_sig=pupil_sig(params.deterend.filtLen/2:end);
 
 %find remaining NaNs
 
-toremove=find(all(~isnan(blue_parcels))==0); % find indices where there are missing NaNs for all parcels
+toremove=all(~isnan(blue_parcels))==0; % find indices where there are missing NaNs for all parcels
 %the indices are always the same as find(all(isnan(blue_parcels))==1) since
 %nans are usually missing from all parcels at once
-pupil_sig(toremove == 1) = [];
 
-face_PC1(toremove == 1,:) = [];
+pupil_sig(toremove== 1) = [];
 
-wheel(toremove == 1) = [];
+face_PC1(toremove== 1,:) = [];
+wheel(toremove== 1)= [];
 
 blue_parcels_z(:,toremove == 1) = [];
 
@@ -194,6 +209,7 @@ blue_parcels(:,toremove == 1) = [];
 %1) using all behavioral variables GRABS:
 
 % regular regression:
+
 rsquared_grabs_allVars = zeros(1,size(blue_parcels,1)); % initialize variable to hold parcel rsqrd vals
 for parcel_num = 1:size(blue_parcels,1) % for each parcel
     y = blue_parcels_z(parcel_num,:)';
@@ -361,37 +377,37 @@ for parcel_num = 1:size(blue_parcels,1) % for each parcel
 end
 
 %Ridge regression:
-rsquared_grabs_face_ridge = zeros(1,size(blue_parcels,1)); % initialize variable to hold parcel rsqrd vals
-for parcel_num = 1:size(blue_parcels,1) % for each parcel
-    y = blue_parcels_z(parcel_num,:)';
-    X = face_PC1;
-    K = 10; % number of folds
-    cv = cvpartition(size(y,1),'kfold',K);
-    cv_rsquared_values = zeros(K,size(y,2));
-    for i = 1:K
-        %training and testing indices for this fold
-        trainIdx = cv.training(i);
-        testIdx = cv.test(i);
-        %return beta dims on training set:
-        [~, betas] = ridgeMML(y(trainIdx,:), X(trainIdx,:), true); %get ridge penalties and beta weights.
-        %predict using testing data
-        Vm = (X(testIdx,:) * betas); % reconstruct data
-        all_rsquareds = zeros(1,size(y,2));
-        for j = 1:size(y,2)
-            predicted = Vm(:,j);
-            actual = y(testIdx,:);
-            actual = actual(:,j);
-            %caluculate R squared
-            SSR = sum((predicted-mean(actual)).^2);
-            SSTotal = sum((actual-mean(actual)).^2);
-            all_rsquareds(j) = SSR/SSTotal;
-        end
-        cv_rsquared_values(i,:) = all_rsquareds;
-    end
-    cvrsquared = mean(cv_rsquared_values);
-    rsquared_grabs_face_ridge(parcel_num) = cvrsquared;
-end
-%}
+% rsquared_grabs_face_ridge = zeros(1,size(blue_parcels,1)); % initialize variable to hold parcel rsqrd vals
+% for parcel_num = 1:size(blue_parcels,1) % for each parcel
+%     y = blue_parcels_z(parcel_num,:)';
+%     X = face_PC1;
+%     K = 10; % number of folds
+%     cv = cvpartition(size(y,1),'kfold',K);
+%     cv_rsquared_values = zeros(K,size(y,2));
+%     for i = 1:K
+%         %training and testing indices for this fold
+%         trainIdx = cv.training(i);
+%         testIdx = cv.test(i);
+%         %return beta dims on training set:
+%         [~, betas] = ridgeMML(y(trainIdx,:), X(trainIdx,:), true); %get ridge penalties and beta weights.
+%         %predict using testing data
+%         Vm = (X(testIdx,:) * betas); % reconstruct data
+%         all_rsquareds = zeros(1,size(y,2));
+%         for j = 1:size(y,2)
+%             predicted = Vm(:,j);
+%             actual = y(testIdx,:);
+%             actual = actual(:,j);
+%             %caluculate R squared
+%             SSR = sum((predicted-mean(actual)).^2);
+%             SSTotal = sum((actual-mean(actual)).^2);
+%             all_rsquareds(j) = SSR/SSTotal;
+%         end
+%         cv_rsquared_values(i,:) = all_rsquareds;
+%     end
+%     cvrsquared = mean(cv_rsquared_values);
+%     rsquared_grabs_face_ridge(parcel_num) = cvrsquared;
+% end
+% %}
 %**************************************************************************
 %**************************************************************************
 %7) using wheel only~ GRABS:
@@ -425,6 +441,7 @@ for parcel_num = 1:size(blue_parcels,1) % for each parcel
     cvrsquared = mean(cv_rsquared_values);
     rsquared_grabs_wheel(parcel_num) = cvrsquared;
 end
+
 % % Ridge regression:
 % rsquared_grabs_wheel_ridge = zeros(1,size(blue_parcels,1)); % initialize variable to hold parcel rsqrd vals
 % for parcel_num = 1:size(blue_parcels,1) % for each parcel
@@ -477,4 +494,9 @@ outputMat_grabs_regular = [outputMat_grabs_regular(:,1:20) nan_parcels nan_parce
 %   outputMat_grabs_ridge(:,21:end) nan_parcels nan_parcels nan_parcels nan_parcels];
 outputMat_grabs_ridge=[];
 toc
+else
+    outputMat_grabs_regular=[];outputMat_grabs_ridge=[];
+    disp(strcat(animal,num2str(k),'NO DATA'))
+end
+disp(strcat(animal,num2str(k),'done'))
 end
